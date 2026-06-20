67736e2ef9c17c48bdde01f8431a15467c278afe
devsWithRafi <mrrafiwebdev@gmail.com>
2026-06-20 16:09:37 +0600

    Add: Get single Lesson routes

---
+++ server/src/controllers/lesson/getSingleLesson.controller.js
@@ -0,0 +1,17 @@
+import { validateLessonBodyData } from '../../helpers/validateLessonBodyData.js';
+import { Lesson } from '../../models/lessonModel.js';
+import '../../models/userModel.js';
+
+export const getSingleLesson = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const lesson = await Lesson.findById(id).populate('author');
+
+    return res.status(200).json({
+      success: true,
+      data: lesson,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
--- server/src/routes/lessonsRoute.js
+++ server/src/routes/lessonsRoute.js
@@ -3,11 +3,13 @@ import { authMiddlewere } from '../middleware/authMiddleware.js';
 import { addNewLesson } from '../controllers/lesson/addNewLesson.controller.js';
 import { getAllLessons } from '../controllers/lesson/getAllLessons.controller.js';
 import { getFeaturedLessons } from '../controllers/lesson/getFeaturedLessons.controller.js';
+import { getSingleLesson } from '../controllers/lesson/getSingleLesson.controller.js';
 
 const lessonsRouter = Router();
 
 lessonsRouter.post('/create', authMiddlewere, addNewLesson);
 lessonsRouter.get('/all', getAllLessons);
 lessonsRouter.get('/featured', getFeaturedLessons);
+lessonsRouter.get('/:id', authMiddlewere, getSingleLesson);
 
 export default lessonsRouter;
